6deb1444b66ffba2b02f8d9ea69564c8ef94884f
harshumaretiya <harsh.patel.hp846@gmail.com>
2021-04-18 23:04:19 +0530

    feat: mimemagic error resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-def index
-  if not user_signed_in?
-    redirect_to user_session_path
+  def index
+    if not user_signed_in?
+      redirect_to user_session_path
+    end
   end
-end
 end
